3edd8abe47f734380678c6d8deaac1009e67a0cb
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2013-06-05 01:40:15 -0400

    changes

--- quickr.psm1
+++ quickr.psm1
@@ -12,6 +12,20 @@ function Merge-Tokens($template, $tokens)
       })    
 }
 
+function Convert-EntriesToObject {
+  BEGIN{ 
+    $Object = New-Object PSObject
+  }
+  PROCESS {
+    foreach ($entry in $input) {
+      $Object | add-member Noteproperty $entry.title.'#text' $entry.content.src
+    }
+  }
+  END{
+    $Object
+  }
+}
+
 function Set-Quickr {
   param(
     [parameter(Mandatory = $true)]
@@ -53,40 +67,40 @@ function New-QuickrPlace {
   )
   
   $url = "$PQ_BASE/LotusQuickr/LotusQuickr/CreateHaiku.nsf?OpenDatabase&PresetFields=h_SetEditCurrentScene;h_CreateManager,h_EditAction;h_Next,h_SetCommand;h_CreateOffice,h_PlaceTypeName;,h_Name;$place,h_UserName;$owner,h_SetPassword;$owner_password,h_EmailAddress;,h_SetReturnUrl;$PQ_BASE/$place,h_OwnerAuth;h_External"
-  Invoke-WebRequest -Uri $url -Headers $PQ_HEADERS
+  $res = Invoke-WebRequest -Uri $url -Headers $PQ_HEADERS
+  if($res.StatusCode -eq 200) {
+    Get-QuickrPlace $place
+  }
+  else {
+    $res
+  }
 }
 
-function New-QuickrRootFolder {
+function Get-QuickrLibraries {
+  $url = "$PQ_BASE/dm/atom/libraries/feed"
+  ([xml](Invoke-WebRequest -Uri $url -Headers $PQ_HEADERS).Content).feed.entry | Convert-EntriesToObject
+}
+
+function Get-QuickrPlace {
   param(
     [parameter(Mandatory = $true)]
-    [string] $place,
-    [parameter(Mandatory = $true)]
-    [string] $name
+    [string] $place
   )
-  $url = "$PQ_BASE/dm/atom/library/%5B@P$place/@RMain.nsf%5D/feed"
-  $template = [string](Get-Content "$PQ_DIR\xml\create_folder.xml")
-  $body = Merge-Tokens $template @{ url = $url; name = $name}
-  
-  (Invoke-WebRequest -Uri $url -Method Post -Body $body -ContentType "application/atom+xml" -Headers $PQ_HEADERS).Content
+  $url = iex "(Get-QuickrLibraries).$place"
+  ([xml](Invoke-WebRequest -Uri $url -Headers $PQ_HEADERS).Content).feed.entry | Convert-EntriesToObject
 }
 
 function New-QuickrFolder {
   param(
-    [parameter(Mandatory = $true, ValueFromPipeline=$true)]
-    [xml[]] $folders,
+    [parameter(Mandatory = $true)]
+    [string] $parentUrl,
     [parameter(Mandatory = $true)]
     [string] $name
   )
-  BEGIN{ 
-    $template = [string](Get-Content "$PQ_DIR\xml\create_folder.xml")
-  }
-  PROCESS {
-    foreach ($f in $folders) {
-      $url = "$PQ_BASE$($f.entry.content.src)"
-      $body = Merge-Tokens $template @{ base = $url; name = $name}
-      (Invoke-WebRequest -Uri $url -Method Post -Body $body -ContentType "application/atom+xml" -Headers $PQ_HEADERS).Content
-    }
-  }
+  $url = "$PQ_BASE$parentUrl"
+  $template = [string](Get-Content "$PQ_DIR\xml\create_folder.xml")
+  $body = Merge-Tokens $template @{ base = $url; name = $name}
+  ([xml](Invoke-WebRequest -Uri $url -Method Post -Body $body -ContentType "application/atom+xml" -Headers $PQ_HEADERS).Content).entry | Convert-EntriesToObject
 }
 
 
@@ -112,6 +126,8 @@ function New-QuickrDocument {
 }
 
 export-modulemember -function Set-Quickr
+export-modulemember -function Get-QuickrLibraries
+export-modulemember -function Get-QuickrPlace
 export-modulemember -function New-QuickrPlace
 export-modulemember -function New-QuickrRootFolder
 export-modulemember -function New-QuickrFolder
